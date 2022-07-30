069340082df59787c8d917b701f1020df6b64cad
Lilia Diaz <liliayamamoto88@gmail.com>
2022-07-30 07:33:05 -1000

    added winning message and button styles

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
 function createNewGame(player1, player2) {
+ 
   return {
     gameOver: false,
-    player1: player1, 
-    player2: player2, 
+    player1: player1,
+    player2: player2,
+    computer: false,
     currentPlayer: player1,
     playerPositionRow: null,
     playerPositionCol: null,
     tie: false,
+    name1:null,
+    name2:null,
+
+    random: function(){
+      let randomNum = Math.floor(Math.random() * 2) + 1;
+      if (randomNum === 1) {
+      this.currentPlayer= player1
+      }
+      if(randomNum===2){
+        this.currentPlayer= player2
+      }
+    },
+
+    computerStarts: function(){
+      if (this.computer){
+        this.playerPositionRow=1;
+        this.playerPositionCol=1;
+      }
+    },
 
     updateBoard: function () {
+
       if (
-        this.gameBoard[this.playerPositionRow][this.playerPositionCol] === null 
+        this.gameBoard[this.playerPositionRow][this.playerPositionCol] === null
       ) {
         this.gameBoard[this.playerPositionRow][this.playerPositionCol] =
           this.currentPlayer;
-          } 
-      else {
-        return "please pick another spot";
-      }
+      } 
+      this.updateUIGameboard();
       this.checkWinner([this.playerPositionRow][this.playerPositionCol]);
-     
     },
+
+    updateUIGameboard: function(){
+
+      let cellId = `${this.playerPositionRow}${this.playerPositionCol}`
+      if (document.getElementById(cellId).innerHTML===''){
+        document.getElementById(cellId).innerHTML=this.currentPlayer;
+      }
+    },
+
     gameBoard: [
       [null, null, null],
       [null, null, null],
@@ -34,7 +62,7 @@ function createNewGame(player1, player2) {
           this.gameBoard[i][1] === this.currentPlayer &&
           this.gameBoard[i][2] === this.currentPlayer
         ) {
-          return this.gameOver = true;
+          return (this.gameOver = true);
         }
         for (let j = 0; j < 3; j++) {
           if (
@@ -42,7 +70,7 @@ function createNewGame(player1, player2) {
             this.gameBoard[1][j] === this.currentPlayer &&
             this.gameBoard[2][j] === this.currentPlayer
           ) {
-            return this.gameOver = true;
+            return (this.gameOver = true);
           }
         }
         if (
@@ -50,142 +78,184 @@ function createNewGame(player1, player2) {
           this.gameBoard[1][1] === this.currentPlayer &&
           this.gameBoard[2][2] === this.currentPlayer
         ) {
-          return this.gameOver = true;
+          return (this.gameOver = true);
         }
         if (
           this.gameBoard[0][2] === this.currentPlayer &&
           this.gameBoard[1][1] === this.currentPlayer &&
           this.gameBoard[2][0] === this.currentPlayer
         ) {
-          return this.gameOver = true;
+          return (this.gameOver = true);
         }
       }
-      this.checkDraw(this.gameBoard)
-      
+      this.checkDraw(this.gameBoard);
     },
 
-    checkDraw: function(){
-      console.log(this.gameBoard)
-      for (let i = 0; i < 3; i++){
-          if (this.gameBoard[i].includes(null)){
-            return this.changePlayer()  
-          }
+    checkDraw: function () {
+      for (let i = 0; i < 3; i++) {
+        if (this.gameBoard[i].includes(null)) {
+          return this.changePlayer();
         }
-        this.tie = true
-      },
+      }
+      this.tie = true;
+    },
 
     changePlayer: function () {
       if (this.currentPlayer === player1) {
         this.currentPlayer = player2;
-       return currentTurn.innerText = `${player2input.value}'s turn`
+        if (this.computer){
+          this.generateMove();
+         // this.currentPlayer = player1;
+        }
       } else {
         this.currentPlayer = player1;
-        return currentTurn.innerText= `${player1input.value}'s turn`
       }
-
     },
+
+    generateMove: function(){
+      if (this.computer){
+      for (let i=0; i<3; i++){
+        for(let j=0; j<3; j++){
+          if (this.gameBoard[i][j]=== null){
+            this.playerPositionRow= i;
+            this.playerPositionCol= j;
+          }
+        }
+      }
+      game.updateBoard();
+    }
+    }
   };
 }
 
-
-let cell = document.querySelectorAll(".cell");
-let board = document.getElementById("board");
-let playerX = "X";
-let playerO = "O";
+const cell = document.querySelectorAll(".cell");
+const board = document.getElementById("board");
+const playerX = "X";
+const playerO = "O";
 let currentPlayer = playerX;
 let gameMessage = document.getElementById("gameMessage");
-let startBtn = document.getElementById("startButton");
-let resetBtn = document.getElementById("resetButton");
-let player1input = document.getElementById("player1");
-let player2input = document.getElementById("player2");
-let player1Name = document.querySelector("#player1Text");
-let player2Name = document.querySelector("#player2Text");
-let currentTurn = document.getElementById("currentTurn");
-if ()
+const startBtn = document.getElementById("startButton");
+const resetBtn = document.getElementById("resetButton");
+const player1input = document.getElementById("player1");
+const player2input = document.getElementById("player2");
+const player1Text = document.querySelector("#player1Text");
+const player2Text = document.querySelector("#player2Text");
+const currentTurn = document.getElementById("currentTurn");
 
 let game = createNewGame(playerX, playerO);
 
-startBtn.addEventListener("click", hide);
-
-function hide() {
-  player1Name.className = "hide";
-  player2Name.className = "hide";
-
-  startBtn.className = "hide";
+function initializeGame(){
+  game.random();
+  if (player2input.value==='') {
+    player2input.value = "computer";
+    game.computer=true
+     if (game.currentPlayer === playerO)
+     {
+        document.getElementById("11").innerHTML= game.player2;
+        game.gameBoard[1][1]=game.player2;
+        game.currentPlayer= game.player1
+        currentTurn.innerText= "computer Starts"
+     }
+  }
+  game.name1=player1input.value
+  game.name2=player2input.value
+  console.log(game.name1)
+  if (game.currentPlayer===playerX){
+    currentTurn.innerText=`${player1input.value} starts`
+  }
+  else if (game.currentPlayer===playerO && game.computer===false){
+    currentTurn.innerText=`${player2input.value} starts`
+  }
   gameMessage.innerText = `${player1input.value} VS. ${player2input.value}`;
 }
-startBtn.addEventListener("click", function(){
-  board.classList.remove("hide")
-  randomPlayer();
+
+startBtn.addEventListener("click", hide);
+
+function resetGame() {
+  for (let i = 0; i < cell.length; i++) {
+    cell[i].innerHTML = "";
+  }
+  gameMessage.innerText = "";
+  currentTurn.innerText = "";
+  startBtn.classList.remove("hide");
+  player1input.value = "";
+  player2input.value = "";
+  player1Text.classList.remove("hide");
+  player2Text.classList.remove("hide");
+  board.classList.add("hide");
+
+  game = createNewGame(playerX, playerO);
+}
+
+function hide() {
+  player1Text.className = "hide";
+  player2Text.className = "hide";
+  startBtn.className = "hide";
+  game.random();
+  if (player2input.value==='') {
+    player2input.value = "computer";
+    game.computer=true
+     if (game.currentPlayer === playerO)
+     {
+        document.getElementById("11").innerHTML= game.player2;
+        game.gameBoard[1][1]=game.player2;
+        game.currentPlayer= game.player1
+        currentTurn.innerText= "computer Starts"
+     }
+     else if (game.currentPlayer === playerX){
+      currentTurn.innerText=`${player1input.value} starts`
+     }
+
+  }
+  game.name1=player1input.value
+  game.name2=player2input.value
+  console.log(game.name1)
+  if (game.currentPlayer===playerX && game.computer===false){
+    currentTurn.innerText=`${player1input.value} starts`
+  }
+  else if (game.currentPlayer===playerO && game.computer===false){
+    currentTurn.innerText=`${player2input.value} starts`
+  }
+  gameMessage.innerText = `${player1input.value} VS. ${player2input.value}`;
+}
+
+
+startBtn.addEventListener("click", function () {
+  board.classList.remove("hide");
 });
 
 
+
 board.addEventListener("click", function (event) {
- if (event.target.innerText === ''){
-  event.target.innerText = game.currentPlayer;
- }
-
- if (currentTurn.innerText === `${player1input.value}'s turn`){
-  currentTurn.innerText = `${player2input.value}'s turn`
- }
- else{currentTurn.innerText= `${player1input.value}'s turn`}
-
- if (game.gameOver){
-  event.target.innerText = ""
- }
+  if (game.gameOver) {
+    return;
+  }
+ 
   let playerRow = event.target.id[0];
   let playerCol = event.target.id[1];
   game.playerPositionRow = playerRow;
   game.playerPositionCol = playerCol;
-  
+
   game.updateBoard();
-  
-  //console.log(game.gameBoard)
+ 
   if (game.gameOver) {
-    gameMessage.innerText= `winner is ${game.currentPlayer}`
-    resetBtn.classList.remove("hide")
-    board.disabled = true
-    currentTurn.innerText=""
+    gameMessage.innerText = `winner is ${game.currentPlayer}`;
+    if (gameMessage.innerText === `winner is X`)
+    {currentTurn.innerText = `winner is ${game.name1}`
+    }
+    else if (gameMessage.innerText === `winner is O`)
+    {currentTurn.innerText = `winner is ${game.name2}`
+    }
 
+   if (game.tie) {
+    gameMessage.innerText = "draw";
+    currentTurn.innerText = "";
+    }
   }
-  console.log(game.tie)
-
-  if (game.tie){
-    gameMessage.innerText= "draw"
-    resetBtn.classList.remove("hide")
-    currentTurn.innerText=""
-  }
+  resetBtn.classList.remove("hide");
 });
 
-resetBtn.addEventListener("click", function(){
-  for(let i = 0; i<cell.length; i++){
-  cell[i].innerHTML= ''
-  }
-  gameMessage.innerText= ""
-  currentTurn.innerText = ""
-  startBtn.classList.remove("hide")
-  player1input.value = ""
-  player2input.value = ""
-  player1Name.classList.remove("hide");
-  player2Name.classList.remove("hide");
-  board.classList.add("hide")
-
-
-  game = createNewGame(playerX, playerO);
-})
-
-
-function randomPlayer(){
-  let randomNum = Math.floor(Math.random() * 2)+1 ;
-  if (randomNum===1){
-    currentTurn.innerText= `${player1input.value}'s turn`
-  }
-
-  if (randomNum===2){
-    currentTurn.innerText= `${player2input.value}'s turn`
-  }
-  
-}
-
-
+resetBtn.addEventListener("click", function () {
+  resetGame();
+});
 
